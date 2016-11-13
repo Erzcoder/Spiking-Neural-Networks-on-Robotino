87eaf8b98dd6cccc0e8989553f0fea18a3e310d4
Nicolas Berberich <n.berberich@gmail.com>
2016-11-13 13:47:58 +0100

    packages added; opencv not working

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/nicolas/Spiking-Neural-Networks-on-Robotino/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel;/home/nicolas/erz_ws/devel".split(';') if "/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel;/home/nicolas/erz_ws/devel" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel/lib/x86_64-linux-gnu:/home/nicolas/erz_ws/devel/lib/x86_64-linux-gnu:/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel/lib:/home/nicolas/erz_ws/devel/lib"
+export PKG_CONFIG_PATH="/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel/lib/x86_64-linux-gnu/pkgconfig:/home/nicolas/erz_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/nicolas/Spiking-Neural-Networks-on-Robotino/devel/lib/pkgconfig:/home/nicolas/erz_ws/devel/lib/pkgconfig"
+export PWD="/home/nicolas/Spiking-Neural-Networks-on-Robotino/build"
