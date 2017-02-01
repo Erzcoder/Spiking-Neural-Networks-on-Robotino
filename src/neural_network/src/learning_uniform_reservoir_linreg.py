edd289bb58c46a04e498a4786dffa365e11a798f
ubuntu <ubuntu@mail.com>
2017-02-01 18:16:58 +0100

    Can't get linear regression to work with uniform network. Linear regression gives zero weights

--- src/neural_network/src/learning_uniform_reservoir_linreg.py
+++ src/neural_network/src/learning_uniform_reservoir_linreg.py
@@ -19,8 +19,8 @@ def gaussian_convolution(spikes,dt):
     mean_rate = np.mean(gauss_rate)
     return mean_rate
 
-
-
+def spike_mean_rate(spikes, sim_time):
+	return len(spikes) / sim_time
 
 def generate_testImage(direction):
 	potential = 10
@@ -36,12 +36,12 @@ def generate_testImage(direction):
 # Labeled image has the form (image, label)
 # Label is a list [on1, on2], on# being the correct value for 
 # the output neurons
-def generate_labeledImages():
+def generate_labeledImages(nr):
 	labeledImages = []
-	for i in range(10):
+	for i in range(nr):
 		labeledImages.append((generate_testImage("right"), [10,0]))
-		labeledImages.append((generate_testImage("middle"), [0,0]))
-		labeledImages.append((generate_testImage("left"), [0,10]))
+		#labeledImages.append((generate_testImage("middle"), [0,0]))
+		#labeledImages.append((generate_testImage("left"), [0,10]))
 
 
 	return labeledImages
@@ -52,11 +52,12 @@ seed=8658764
 
 input_nr = 9
 readout_nr = 2
-exc_nr = 20
-inh_nr = 5
+reservoir_nr = 1
+#exc_nr = 20
+#inh_nr = 5
 
 simulation_time = 50.0
-dt = 1
+dt = 0.1
 
 p.setup(timestep=dt) # 0.1ms
 
@@ -65,19 +66,17 @@ p.setup(timestep=dt) # 0.1ms
 
 ###### Neurons #######
 
-input_neurons = p.Population(input_nr, p.SpikeSourcePoisson(rate=0))
+input_neurons = p.Population(input_nr, p.SpikeSourcePoisson())
 readout_neurons = p.Population(readout_nr, p.IF_curr_exp, {}, label="readout")
 
-reservoir_exc = p.Population(exc_nr,p.IF_curr_exp, {}, label="reservoir_exc")
-reservoir_inh = p.Population(inh_nr,p.IF_curr_exp, {}, label="reservoir_inh")
-
+reservoir = p.Population(reservoir_nr,p.IF_curr_exp, {}, label="reservoir")
 ######################
 
 ###### Synapses #######
 
-stat_syn_exc = p.StaticSynapse(weight =5.0, delay=1)
-stat_syn_inh = p.StaticSynapse(weight =20.0, delay=1)
+stat_syn_res = p.StaticSynapse(weight =5.0, delay=1)
 stat_syn_input = p.StaticSynapse(weight =50.0, delay=1)
+stat_syn_rout = p.StaticSynapse(weight =0.0, delay=1)
 
 ######################
 
@@ -86,79 +85,62 @@ stat_syn_input = p.StaticSynapse(weight =50.0, delay=1)
 pconn = 0.01      # sparse connection probability
 
 rng = NumpyRNG(seed=seed)
-exc_conn = p.FixedProbabilityConnector(pconn, rng=rng)
-inh_conn = p.FixedProbabilityConnector(pconn, rng=rng)
+res_conn = p.FixedProbabilityConnector(pconn, rng=rng)
+
 inp_conn = p.AllToAllConnector()
 rout_conn = p.AllToAllConnector()
 
 connections = {}
-connections['e2e'] = p.Projection(reservoir_exc, reservoir_exc, exc_conn,
-                                synapse_type=stat_syn_exc, receptor_type='excitatory')
-connections['e2i'] = p.Projection(reservoir_exc, reservoir_inh, exc_conn,
-                                synapse_type=stat_syn_exc,receptor_type='excitatory')
-connections['i2e'] = p.Projection(reservoir_inh, reservoir_exc, inh_conn,
-                                synapse_type=stat_syn_inh,receptor_type='inhibitory')
-connections['i2i'] = p.Projection(reservoir_inh, reservoir_inh, inh_conn,
-                                synapse_type=stat_syn_inh,receptor_type='inhibitory')
+connections['r2r'] = p.Projection(reservoir, reservoir, res_conn,
+                                synapse_type=stat_syn_res, receptor_type='excitatory')
 
-connections['inp2e'] = p.Projection(input_neurons, reservoir_exc, inp_conn,
-                                      synapse_type=stat_syn_input,receptor_type='excitatory')
-connections['inp2i'] = p.Projection(input_neurons, reservoir_inh, inp_conn,
+connections['inp2r'] = p.Projection(input_neurons, reservoir, inp_conn,
                                       synapse_type=stat_syn_input,receptor_type='excitatory')
 
-connections['e2rout'] = p.Projection(reservoir_exc, readout_neurons, rout_conn,
-                                      synapse_type=stat_syn_exc,receptor_type='excitatory')
-connections['i2rout'] = p.Projection(reservoir_inh, readout_neurons, rout_conn,
-                                      synapse_type=stat_syn_inh,receptor_type='inhibitory')
+connections['r2rout'] = p.Projection(reservoir, readout_neurons, rout_conn,
+                                      synapse_type=stat_syn_rout,receptor_type='excitatory')
 
 
 ######################
 
 ####### Feed images to network and record #######
-
-labeledImages = generate_labeledImages()
+images_nr = 10
+labeledImages = generate_labeledImages(images_nr)
 
 input_neurons.record(['spikes'])
-reservoir_exc.record(['spikes'])
-reservoir_inh.record(['spikes'])
+reservoir.record(['spikes'])
 readout_neurons.record(['spikes'])
 
 # 30 images x 25 neurons, contains avarage nr of spikes for each neuron 
-X = np.zeros( (30,25) )
+X = np.zeros( (images_nr,reservoir_nr) )
 # yi, expected nr of spikes for output neurons 
 y1 = []
 y2 = []
 i = 0
 for labeledImage in labeledImages:
-	# ISSUE, can't feed in different images. 
-	# WANTED: input_neurons.set(rate=labeledImage[0])
-	print('Image')
-	print(labeledImages[0][0])
-	input_neurons.set(rate=labeledImages[0][0])
+	#print('Image')
+	#print(labeledImage[0])
+	input_neurons.set(rate=labeledImage[0])
 
 	p.run(simulation_time)
 
 
+	readout_neurons_data = readout_neurons.get_data(clear=True)
+	#print("rout 1")
+	#print(readout_neurons_data.segments[0].spiketrains[0])
+	#print("rout 2")
+	#print(readout_neurons_data.segments[0].spiketrains[1])
 
-	reservoir_exc_data = reservoir_exc.get_data()
-	reservoir_inh_data = reservoir_inh.get_data()
-	input_neurons_data = input_neurons.get_data()
+	reservoir_data = reservoir.get_data(clear=True)
+	#print(reservoir_data.segments[0].spiketrains[0])
+	input_neurons_data = input_neurons.get_data(clear=True)
 
-	#spikes_exc = reservoir_exc_data.segments[0].spiketrains[0]
-	# spikes_inh = reservoir_inh_data.segments[0].spiketrains[0]
-	#spikes_input = input_neurons_data.segments[0].spiketrains[0]
-	#print('Input neruons spiking')
-	#print(spikes_input)
-	#print('Exc neruons spiking')
-	#print(spikes_exc)
 
+	#print('reservoir')
+	#print(reservoir_data.segments[0].spiketrains[0])
 	mean_rates = []
-	for spiketrain in reservoir_exc_data.segments[0].spiketrains:
-		mean_rate = len(spiketrain)/simulation_time
-		mean_rates.append(mean_rate)
-
-	for spiketrain in reservoir_inh_data.segments[0].spiketrains:
-		mean_rate = len(spiketrain)/simulation_time
+	for spiketrain in reservoir_data.segments[0].spiketrains:
+		mean_rate = spike_mean_rate(spiketrain, simulation_time)
 		mean_rates.append(mean_rate)
 
 	X[i] = mean_rates
@@ -178,7 +160,6 @@ print(y2)
 
 
 ######### Fit weights to each output neuron with linear regression ###########
-
 
 regr = linear_model.LinearRegression()
 
@@ -201,45 +182,41 @@ print(w2)
 
 ######### Test accuracy ###########
 
+print("\nTesting accuracy\n")
+print("Test Image")
 image = labeledImages[0][0]
+print(image)
 
 input_neurons.set(rate=image)
 p.run(simulation_time)
 
-readout_neurons_data = readout_neurons.get_data()
+readout_neurons_data = readout_neurons.get_data(clear=True)
+strains = readout_neurons_data.segments[0].spiketrains
 
-print('Output neuron 1 before')
-print(len(readout_neurons_data.segments[0].spiketrains[0]))
-
-#print(len(w1[0:exc_nr]))
-#print(len(connections['e2rout'].get('weight', format='list')))
-#print('W before')
-#print(connections['e2rout'].get('weight', format='list'))
-
-# Connection['e2rout'] looks like [n1, n2, value]
-w_exc = []
-for i in range(exc_nr):
-	w_exc.append(w1[i])
-	w_exc.append(w2[i])
+print('Mean rate output neurons before change of weights')
+print('(' + str(spike_mean_rate(strains[0], simulation_time)) + \
+',' + str(spike_mean_rate(strains[1], simulation_time)) + ')')
 
 
-w_inh = []
-for i in range(inh_nr):
-	w_inh.append(w1[exc_nr + i])
-	w_inh.append(w2[exc_nr + i])
+# Connection['r2rout'] looks like
+# [ [r0, rout0, value], [r0, rout1, v], [r1, rout0, v] ... ]
+w = []
+for i in range(reservoir_nr):
+	w.append(w1[i])
+	w.append(w2[i])
 
-
-print('Before set connection')
-#connections['e2rout'].set(weight=w_exc)
-connections['i2rout'].set(weight=w_inh)
-
-#print('W after')
-#print(connections['e2rout'].get('weight', format='list'))
+connections['r2rout'].set(weight=w)
 
 p.run(simulation_time)
 
-readout_neurons_data = readout_neurons.get_data()
+#reservoir_data = reservoir.get_data(clear=True)
+#print(reservoir_data.segments[0].spiketrains[0])
 
-print('Output neuron 1 after')
-print(len(readout_neurons_data.segments[0].spiketrains[1]))
+readout_neurons_data = readout_neurons.get_data(clear=True)
+strains = readout_neurons_data.segments[0].spiketrains
+#print(strains[0])
+
+print('Mean rate output neurons after change of weights')
+print('(' + str(spike_mean_rate(strains[0], simulation_time)) + \
+',' + str(spike_mean_rate(strains[1], simulation_time)) + ')')
 
