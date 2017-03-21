ca0f0528033ab59107d26cb3e89e96f0b87779e0
bogdan-paulis <bogdan.paulis@tum.de>
2017-03-21 07:22:41 +0100

    Add files via upload
    files for learning:
    network_command formats the teleop commands
    gazebo_common saves data in format [image,label]

---
+++ src/neural_network/src/gazebo_common.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import rospy
+import message_filters
+
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+from sensor_msgs.msg import Image
+
+from random import randint
+
+def init():
+    rospy.init_node('data_generator')
+    rate = rospy.Rate(10) # 10hz
+    
+    global nr_train
+    nr_train=18
+    global nr_test
+    nr_test=2
+    global nr_data
+    nr_data=0
+    global train_data
+    train_data = []
+    global test_data
+    test_data = []
+
+    global image_sub
+    image_sub = message_filters.Subscriber('camera/image_processed', Image)
+    #image_sub = message_filters.Subscriber('/camera/rgb/image_raw', Image)
+    global label_sub
+    label_sub = message_filters.Subscriber('network_command', numpy_msg(Floats))
+
+    ts = message_filters.TimeSynchronizer([image_sub, label_sub], 10)
+    ts.registerCallback(callback)
+
+    rospy.loginfo('starting---------------')
+    rospy.spin()
+    
+# Labeled image has the form (image, label)
+# Label is a list [on1, on2], on# being the correct value for 
+# the output neurons
+
+def callback(image,label):
+    nr_data=nr_data+1
+    rospy.loginfo('Captured %d data.',nr_data)
+    if randint(1,nr_train+nr_test)<=nr_test:
+	test_data.append(image,label)
+    else:
+        train_data.append(image,label)
+
+    if nrData==nrMaxData:
+        image_sub.unregister()
+        label_sub.unregister()
+        use_data()
+
+
+
+
+
+
+def use_data():
+    i=randint(1,nr_train)
+    rospy.loginfo('random data label %d: [%f,%f]',i,train_data[1][0],train_data[1][1])
+
+
+
+
+class param:
+	seed = 8658764			# Seed for reproduction of random number
+	#rng = NumpyRNG()		# Use seed to reproduce 
+	input_nr = 9			# Number of input neurons
+	readout_nr = 2			# Number of readout neurons
+	reservoir_nr = 10		# Number of reservour neurons
+	res_exc_nr = 20			# Number of excitatory neurons
+	res_inh_nr = 5			# Number of inhibitory neurons
+	simulation_time = 200.0 # Simulation time for each input
+	dt = 1					# Timestep in simulation
+	res_pconn = 0.1		# sparse connection probability for reservoir
+	images_train_nr = 9		# Number of training images to train with, 
+							# Must be a factor of 3
+	images_test_nr = 9  	# Number of test images
+	#images_train = generate_labeledImages(images_train_nr)
+	#images_test = generate_labeledImages(images_test_nr)
+
+if __name__ == '__main__':
+    try:
+        init()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+
+
---
+++ src/neural_network/src/network_command.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import rospy
+#from std_msgs.msg import String
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+from geometry_msgs.msg import Twist
+
+def init():
+    rospy.init_node('network_command')
+    rate = rospy.Rate(10) # 10hz
+    rospy.Subscriber("/cmd_vel_mux/input/teleop", Twist, test_callback)
+
+    rospy.loginfo('starting---------------')
+    rospy.spin()
+    
+
+def test_callback(data_input):
+   
+    pub = rospy.Publisher('network_command', numpy_msg(Floats), queue_size=10)
+    # construct the output command
+        
+    if data_input.angular.z==0 :
+        msg=np.array([0,0], dtype=np.float32)
+        
+    elif data_input.angular.z>0 :
+        msg=np.array([data_input.angular.z,0], dtype=np.float32)
+        
+    elif data_input.angular.z<0 :
+        msg=np.array([0,-data_input.angular.z], dtype=np.float32)
+    #we need to add timestamps to the data
+    #msg.Header().stamp=rospy.Time.now() 
+
+    pub.publish(msg)
+
+
+if __name__ == '__main__':
+    try:
+        init()
+    except rospy.ROSInterruptException:
+        pass
